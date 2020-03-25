1148ef6f01dbd393d279b6840390a35f6de4b1a9
Batpillet <batpillet@gmail.com>
2020-03-25 01:23:12 +0100

    adding suivi de croissance tab in dashboard

---
+++ app/src/main/java/com/example/babybook/CroissanceView.java
@@ -0,0 +1,14 @@
+package com.example.babybook;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CroissanceView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_croissance_view);
+    }
+}
